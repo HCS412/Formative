65b66d70c3206ce4d1efe151d693769c5d52f182
HCS412 <HCS412@users.noreply.github.com>
2025-12-26 17:08:30 -0500

    Fix: Remove dist/ from git, force Railway fresh build
    - Added dist/ to .gitignore (was commented out)
    - Removed dist/ from git tracking
    - Railway will now build fresh instead of serving stale cached build
    - This fixes the React Router caching issues from before

--- dist/assets/arrow-left-C9yPAY5-.js
+++
@@ -1,8 +0,0 @@
-import{b as o}from"./core-CTluuSil.js";import"./index-BWUzC6n8.js";import"./index.es-u7oFkoQ5.js";import"./events-DQ172AOg.js";import"./index-nibyPLVP.js";import"./useChainId-BbZBUEzh.js";import"./en_US-KAK2ZBDO-BL9qVoL9.js";import"./isAddressEqual-DKYan-C6.js";import"./secp256k1-BuwZWKXW.js";const v=o`<svg fill="none" viewBox="0 0 14 15">
-  <path
-    fill="currentColor"
-    fill-rule="evenodd"
-    d="M13 7.99a1 1 0 0 1-1 1H4.4l2.46 2.46a1 1 0 1 1-1.41 1.41L1.29 8.7a1 1 0 0 1 0-1.41L5.46 3.1a1 1 0 0 1 1.41 1.42L4.41 6.99H12a1 1 0 0 1 1 1Z"
-    clip-rule="evenodd"
-  />
-</svg>`;export{v as arrowLeftSvg};
--- dist/assets/swapVertical-DZ0OCzHr.js
+++
@@ -1,8 +0,0 @@
-import{b as l}from"./core-CTluuSil.js";import"./index-BWUzC6n8.js";import"./index.es-u7oFkoQ5.js";import"./events-DQ172AOg.js";import"./index-nibyPLVP.js";import"./useChainId-BbZBUEzh.js";import"./en_US-KAK2ZBDO-BL9qVoL9.js";import"./isAddressEqual-DKYan-C6.js";import"./secp256k1-BuwZWKXW.js";const n=l`<svg fill="none" viewBox="0 0 14 14">
-  <path
-    fill="currentColor"
-    fill-rule="evenodd"
-    d="M3.48 2.18a1 1 0 0 1 1.41 0l2.68 2.68a1 1 0 1 1-1.41 1.42l-.98-.98v4.56a1 1 0 0 1-2 0V5.3l-.97.98A1 1 0 0 1 .79 4.86l2.69-2.68Zm6.34 2.93a1 1 0 0 1 1 1v4.56l.97-.98a1 1 0 1 1 1.42 1.42l-2.69 2.68a1 1 0 0 1-1.41 0l-2.68-2.68a1 1 0 0 1 1.41-1.42l.98.98V6.1a1 1 0 0 1 1-1Z"
-    clip-rule="evenodd"
-  />
-</svg>`;export{n as swapVerticalSvg};
